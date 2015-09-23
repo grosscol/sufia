11fbd40092c7739e29004d2b29f3558c2458f717
Colin Gross <grosscol@umich.edu>
2015-09-23 17:04:04 -0400

    Update documentation for zotero reload_config.

--- lib/sufia/zotero/config.rb
+++ lib/sufia/zotero/config.rb
@@ -4,6 +4,8 @@ module Sufia
       @config ||= reload_config!
     end
 
+    # parse and set configuration from config/zotero.yml file
+    # @return [Hash] with :client_key and :client_secret 
     def self.reload_config!
       @config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'zotero.yml'))).result)['zotero']
     end
